562a8989dd6d18ccc4c928056330d8ed9d699a7c
student <student@DESKTOP-5577H91>
2017-11-02 10:07:35 +0100

    zadatak

---
+++ ClassLibrary1/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailSender;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailClient MailClient= new MailClient();
+            MailClient.SendEmail("vesna.safranko@vsmti.hr", "proba", "Booook");
+            Console.ReadKey();
+        }
+    }
+}
